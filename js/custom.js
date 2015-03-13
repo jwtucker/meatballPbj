796c3a145c2cb65f2dda8867d799d9b0408265b8
jwtucker <tuckerjeremyw@gmail.com>
2015-03-13 04:17:32 -0400

    Vertical positioning script

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 			$('.window').hide();
 			$('#home' + currentSite).show();
 			$('.pbjMain').fadeIn(600);
+			calculateResize();
 			$('body').toggleClass('purple',600);
 		});
 	});
@@ -27,11 +28,18 @@ $(document).ready(function(){
 			$('.window').hide();
 			$('#home' + currentSite).show();
 			$('.meatballMain').fadeIn(600);
+			calculateResize();
 		});
 	});
 
 	meatballArrowAnimate();
 	pbjArrowAnimate();
+
+	calculateResize();
+
+	$(window).resize(function(){
+		calculateResize();
+	});
 
 });
 
@@ -41,31 +49,35 @@ function checkLinks(){
 		$(".window").hide();
 		$("#home" + currentSite).show();
 		currentSection = "#home";
+		console.log($("#home" + currentSite).height());
 	});
 
 	$('.menuLink').click(function(){
 		$(".window").hide();
-		console.log(currentSite);
 		$("#menu" + currentSite).show();
 		currentSection = "#menu";
+		console.log($("#menu" + currentSite).height());
 	});
 
 	$('.pressLink').click(function(){
 		$(".window").hide();
 		$("#press" + currentSite).show();
 		currentSection = "#press";
+		console.log($("#press" + currentSite).height());
 	});
 
 	$('.bioLink').click(function(){
 		$(".window").hide();
 		$("#bio" + currentSite).show();
 		currentSection = "#bio";
+		console.log($("#bio" + currentSite).height());
 	});
 
 	$('.contactLink').click(function(){
 		$(".window").hide();
 		$("#contact" + currentSite).show();
 		currentSection = "#contact";
+		console.log($("#contact" + currentSite).height());
 	});
 }
 
@@ -78,5 +90,24 @@ function meatballArrowAnimate(){
 function pbjArrowAnimate(){
 	$('.pbjArrow').fadeOut(1500,function(){
 		$('.pbjArrow').fadeIn(1500,pbjArrowAnimate());
+	})
+}
+
+function calculateResize(){
+	if($(window).height() > 900){
+		$('.meatballBarWrapper').css('top',$(window).height()-300);
+		$('.pbjBarWrapper').css('top',$(window).height()-300);
+	}
+	else{
+		$('.meatballBarWrapper').css('top',600);	
+		$('.pbjBarWrapper').css('top',600);	
+	}
+	$('.window').each(function(i){
+		if($(window).height() > 900){
+			$(this).css('margin-top', ($(window).height()-$(this).height())/2);
+		}
+		else{
+			$(this).css('margin-top', (900-$(this).height())/2);
+		}
 	})
 }
